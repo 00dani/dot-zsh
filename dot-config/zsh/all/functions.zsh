52cda3f34ee4a2d14d7ba67e91a4d204adee6960
Danielle McLean <dani@00dani.me>
2023-12-05 21:30:58 +1100

    Add 'dump' func, which shows you its arguments quoted

--- dot-config/zsh/all/functions.zsh
+++ dot-config/zsh/all/functions.zsh
@@ -3,5 +3,6 @@
 # inherited in the environment so non-login shells need them, and you'll want
 # to call one in a script so non-interactive shells need them.
 autoload count
+autoload dump
 autoload enphp
 autoload -Uz zargs zcalc zmv
